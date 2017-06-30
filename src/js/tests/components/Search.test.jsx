d0ccf85195b31442d0fbc2e0fe10f4c5f5084cd8
Chaitya Shah <Chaitya62@gmail.com>
2017-07-01 00:02:39 +0530

    Add Tests for Search Component

--- src/js/tests/components/Search.test.jsx
+++ src/js/tests/components/Search.test.jsx
@@ -11,4 +11,24 @@ describe('Search', () => {
     expect(Search).toExist();
   });
 
+  it('should call onSearch props with entered input text', () => {
+    const searchText = 'Search this';
+    var spy = expect.createSpy();
+    var search = ReactTestUtils.renderIntoDocument(<Search onSearch={spy}/>);
+
+    search.refs.searchText.value = searchText;
+    ReactTestUtils.Simulate.change(search.refs.searchText);
+    expect(spy).toHaveBeenCalledWith(false, searchText);
+  });
+
+  it('should call onSearch with proper checked value', () => {
+    var spy = expect.createSpy();
+    var search = ReactTestUtils.renderIntoDocument(<Search onSearch={spy}/>);
+
+    search.refs.showCompleted.checked = true;
+    ReactTestUtils.Simulate.change(search.refs.showCompleted);
+    expect(spy).toHaveBeenCalledWith(true, '');
+
+  });
+
 });
